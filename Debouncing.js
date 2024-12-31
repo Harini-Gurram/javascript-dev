112966f41ce3b18ca99e5c5a119a89046c94bef9
Harini-Gurram <harinigurram2001@gmail.com>
2024-12-31 16:55:12 +0530

    Comment the return function

---
+++ Debouncing.js
@@ -0,0 +1,33 @@
+function doMagic(func, context) {
+  func.apply(context); //applies the user context(this) to the current functon to access
+}
+const user = {
+  name: "Alice",
+  sayHello: function greeting(arg) {
+    // const args = arg;
+    return doMagic(function () {
+      console.log(arg + this.name); //remembers the variable arg due to closures concept (The callback function inside doMagic is binded to the closure of greeting)
+      // this.sayHello(arg); //infinitely calls the greeting function
+    }, this); //here this refers to the user object context like name and sayHello
+  },
+};
+user.sayHello("Hello");
+
+let cnt = 0;
+function getData() {
+  console.log("Fetching data", cnt++);
+}
+function debounce(func, delay) {
+  let timer;
+  // return function () {
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    // func.apply(this, args);
+    func();
+  }, delay);
+  // };
+}
+
+// If debounce function doesn't return any function then handleInputChange is undefined so it throws error
+//When you comment out the return function() line in the debounce implementation, the debounce function no longer returns a new function for the event handler.
+const handleInputChange = debounce(getData, 3000);
